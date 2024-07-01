32a3f1f11bf3e8d3d5419774de258bd9b7bbb367
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-07-01 13:29:54 +0100

    time

--- scripts.js
+++ scripts.js
@@ -1,22 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const currentTimeUTC = document.getElementById("currentTimeUTC");
-  const currentDay = document.getElementById("currentDay");
+const currentTimeUTC = document.getElementById("currentTimeUTC");
+const currentDay = document.getElementById("currentDay");
 
-  function updateTime() {
-    const now = new Date();
-    currentTimeUTC.textContent =
-      now.toISOString().split("T")[1].split(".")[0] + " UTC";
-  }
+function updateTime() {
+  const now = new Date();
+  currentTimeUTC.textContent =
+    now.toISOString().split("T")[1].split(".")[0] + " UTC";
+}
 
-  function updateDay() {
-    const now = new Date();
-    const options = { weekday: "long" };
-    currentDay.textContent = new Intl.DateTimeFormat("en-US", options).format(
-      now
-    );
-  }
+function updateDay() {
+  const now = new Date();
+  const options = { weekday: "long" };
+  currentDay.textContent = new Intl.DateTimeFormat("en-US", options).format(
+    now
+  );
+}
 
-  updateDay();
-  updateTime();
-  setInterval(updateTime, 1000);
-});
+updateDay();
+updateTime();
+setInterval(updateTime, 1000);
